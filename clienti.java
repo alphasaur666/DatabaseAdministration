4f23fa4bc8578dec29e6690a8cc79a851fb44182
Roșu Răzvan-Virgil <celtics.razvan@gmail.com>
2018-10-29 20:39:29 +0200

    Update clienti.java

--- clienti.java
+++ clienti.java
@@ -323,7 +323,7 @@ public class clienti extends javax.swing.JFrame {
         try{
         String host ="jdbc:mysql://localhost:3306/MAGAZIN_CD";
         String uName="RAZVAN";
-        String Pass="SEPTIMUS97";
+        String Pass="dogshit";
          con = DriverManager.getConnection( host, uName, Pass );
          stmt=con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
          String sql="SELECT * FROM CLIENTI";
